34fb0dba78d1f5f758b7d33895f8f7c7bddbb1b2
triple-s-rank <specialforwar@icloud.com>
2022-07-09 20:17:23 +0300

    Fork serialize post function

--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,30 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
+def optimized_serialize_post(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.objects.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_post(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': Comment.objects.filter(post=post).count(),
+        'comments_amount': len(Comment.objects.filter(post=post)),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.objects.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -24,13 +38,13 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
+        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
 def index(request):
 
-    most_popular_posts = Post.objects.annotate(Count('likes')).order_by('-likes__count')[:4].prefetch_related('author')
+    most_popular_posts = Post.objects.annotate(Count('likes')).order_by('-likes__count')[:4]
 
     most_fresh_posts = Post.objects.order_by('-published_at')[:4]
 
@@ -57,9 +71,9 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.prefetch_related('likes')
+    likes = post.likes.all()
 
-    related_tags = post.prefetch_related('tags')
+    related_tags = post.tags.all()
 
     serialized_post = {
         'title': post.title,
