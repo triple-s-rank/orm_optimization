aa8c9e93bb9a3968cfcc9414338df0b842d9e259
triple-s-rank <specialforwar@icloud.com>
2022-07-11 15:16:29 +0300

    Add second annotation for comments count in most popular posts

--- blog/views.py
+++ blog/views.py
@@ -7,20 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-def optimized_serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.objects.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -30,7 +16,21 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.objects.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -44,11 +44,11 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = Post.objects.annotate(Count('likes')).order_by('-likes__count')[:4]
+    most_popular_posts = Post.objects.annotate(likes_number=Count('likes'), comments_number=Count('comments')).prefetch_related('author').order_by('-likes_number')[:4]
 
     most_fresh_posts = Post.objects.order_by('-published_at')[:4]
 
-    most_popular_tags = Tag.objects.annotate(Count('posts')).order_by('-posts__count')[:4]
+    most_popular_tags = Tag.objects.annotate(posts_number=Count('posts')).order_by('-posts_number')[:4]
 
     context = {
         'most_popular_posts': [
